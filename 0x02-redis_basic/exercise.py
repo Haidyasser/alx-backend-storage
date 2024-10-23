4db00ae097219c33fd97f8ab89a9bb519de9aa41
Haidyasser <haidyasser3@gmail.com>
2024-10-23 22:40:46 +0300

    decorator to count methods calls

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,7 @@
 import redis
 import uuid
 from typing import Union, Optional
+from functools import wraps
 
 
 class Cache:
@@ -11,13 +12,25 @@ class Cache:
         """ Constructor """
         self._redis = redis.Redis()
         self._redis.flushdb()
+        self.call_count = {}
 
+    def count_calls(self, fn: callable) -> callable:
+        """ Decorator to count calls to a method """
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            name = fn.__name__
+            self.call_count[name] = self.call_count.get(name, 0) + 1
+            return fn(*args, **kwargs)
+        return wrapper
+
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Store data in Redis """
         key = f"data:{uuid.uuid4()}"
         self._redis.set(key, data)
-        return key 
+        return key
 
+    @count_calls
     def get(self, key: str, fn: callable = None) -> Optional[Union[str, bytes, int, float]]:
         """ get data from Redis """
         data = self._redis.get(key)
@@ -25,10 +38,12 @@ class Cache:
             return None
         return fn(data) if fn else data
 
+    @count_calls
     def get_str(self, key: str) -> Optional[str]:
         """ get string from bytes """
         return self.get(key, fn=lambda d: d.decode('utf-8'))
-    
+
+    @count_calls
     def get_int(self, key: str) -> Optional[int]:
         """ get int from bytes """
         return self.get(key, fn=lambda d: int(d))
